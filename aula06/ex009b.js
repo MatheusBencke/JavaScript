a92b5cb69b26be074ceb8620e3733406149e5416
Bencke <mathbencke@gmail.com>
2019-11-12 17:21:37 -0300

    Olha so, to dando multa

---
+++ aula06/ex009b.js
@@ -0,0 +1,6 @@
+var vel = 61
+console.log(`A velocidade do seu carro eh ${vel} Km/h`)
+if(vel > 60){
+    console.log('Vc dirije muito rapido, vc eh o Flash xD. MULTADO!')
+}
+console.log(`Dirija sempre com cinto de segurança`)
---
+++ aula06/ex009c.js
@@ -0,0 +1,5 @@
+var pais = 'França'
+console.log(`Vivendo em ${pais}`)
+if(pais != 'Brasil'){
+    console.log('Un forasteirooo!')
+}
